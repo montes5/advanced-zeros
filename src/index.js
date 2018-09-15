6cbe3c4ab51abfe3fce64ac960650acfd0700067
montes5 <alexmontes5@yandex.ru>
2018-09-16 01:07:13 +0300

    solution1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+	var solution = 0;
+	var maxSM = searchMaxSimpleMultiplier(base);
+	var degreeOfSM = searchDegreeOfSimpleMultiplier(base, maxSM);
+	function searchMaxSimpleMultiplier(num) {
+		var divider = 2;
+		while(num % divider != 0) {
+			divider++;
+        }
+		if(divider == num) return num;
+		else return searchMaxSimpleMultiplier(num/divider);
+    }
+	function searchDegreeOfSimpleMultiplier(num, divider) {
+		var answer = 0;
+		while(num % divider == 0) {
+			num /= divider;
+			answer++;
+        }
+		return answer;
+    }
+	while(number > 0) {
+        number = Math.floor(number/maxSM);
+        solution += number;
+    }
+    return Math.floor(solution/degreeOfSM);
 }
